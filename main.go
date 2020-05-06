a59e17229f73f070645e9c79088a2ce50acb00d3
topd333 <genius.snow0405@gmail.com>
2020-05-06 06:08:12 +0300

    add data model for news API

--- main.go
+++ main.go
@@ -1,14 +1,47 @@
 package main
 
 import (
+    "flag"
     "fmt"
     "html/template"
+    "log"
     "net/http"
     "net/url"
     "os"
+    "time"
 )
 
 var tpl = template.Must(template.ParseFiles("index.html"))
+var apiKey *string
+
+type Source struct {
+    ID   interface{} `json:"id"`
+    Name string      `json:"name"`
+}
+
+type Article struct {
+    Source      Source    `json:"source"`
+    Author      string    `json:"author"`
+    Title       string    `json:"title"`
+    Description string    `json:"description"`
+    URL         string    `json:"url"`
+    URLToImage  string    `json:"urlToImage"`
+    PublishedAt time.Time `json:"publishedAt"`
+    Content     string    `json:"content"`
+}
+
+type Results struct {
+    Status       string    `json:"status"`
+    TotalResults int       `json:"totalResults"`
+    Articles     []Article `json:"articles"`
+}
+
+type Search struct {
+    SearchKey  string
+    NextPage   int
+    TotalPages int
+    Results    Results
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
     tpl.Execute(w, nil)
@@ -37,6 +70,13 @@ func main() {
     port := os.Getenv("PORT")
     if port == "" {
         port = "3000"
+    }
+
+    apiKey = flag.String("apikey", "", "Newsapi.org access key")
+    flag.Parse()
+
+    if *apiKey == "" {
+        log.Fatal("apiKey must be set")
     }
 
     mux := http.NewServeMux()
